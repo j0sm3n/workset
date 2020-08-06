05e75791210154a71e48b4bf09ed976d6540e1f1
Jose Antonio Mendoza <josmen@gmail.com>
2020-08-06 08:04:32 +0200

    Django project started

---
+++ workdays/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for workdays project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workdays.settings')
+
+application = get_wsgi_application()
